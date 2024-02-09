1b6572ca07405470a0ef70c40fd7b3780829cd7e
marcolewis17 <marcolewis17@gmail.com>
2024-02-09 11:23:33 -0700

    Se agregaro info al blog

--- src/Views/Blog/Index.jsx
+++ src/Views/Blog/Index.jsx
@@ -1,8 +1,30 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { sendRequest } from "../../functions";
 
 const Blog = () => {
+  const [entradas, setEntradas] = useState([]);
+
+  useEffect(() => {
+    getEntradas();
+  }, []);
+
+  const getEntradas = async () => {
+    const res = await sendRequest("GET", "", "/entradas", "");
+    setEntradas(res);
+  };
+
   return (
-  <div>Aqui ira un pegueño blog</div>
+    <div>
+      <h2 className=" mt-3 mb-3">Bienvenido al blog</h2>
+      {entradas.map((entrada) => {
+        return (
+          <article key={entrada.id}>
+            <h3>Entrada: {entrada.titulo}</h3>
+            <p>{entrada.contenido}</p>
+          </article>
+        );
+      })}
+    </div>
   );
 };
 
